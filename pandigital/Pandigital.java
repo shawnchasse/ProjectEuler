a1a5b0c9bfb2cc1a9ce37fa5479bc5c69e3cf6b1
Shawn Chasse <shawnchasse@gmail.com>
2013-02-22 16:52:20 -0500

    Solution for problem 43, done using permutations and then checking each pemutation.

--- pandigital/Pandigital.java
+++ pandigital/Pandigital.java
@@ -23,7 +23,78 @@ public class Pandigital
 
       long pandigitalPrime = products.findPandigitalPrime();
       System.out.println("The largest n-digit pandigital prime number is " + pandigitalPrime );
+
+      long pandigitalSumSubStringDivisible = products.findPandigitalSubStringDivisibilitySum();
+      System.out.println("Sum of all 0 to 9 pandigital numbers with special divisibility is " + pandigitalSumSubStringDivisible );
    }
+
+
+   public boolean isSubStringDivisibility(String digitStr )
+   {
+      System.out.print("Checking " + digitStr + "\r");
+      // now if it is do the division
+      // digits 2,3,4 divisible by 2
+      // digits 3,4,5 divisible by 3
+      // digits 4,5,6 divisible by 5
+      // digits 5,6,7 divisible by 7
+      // digits 6,7,8 divisible by 11
+      // digits 7,8,9 divisible by 13
+      // digits 8,9,10 divisible by 17
+      int[] subStart = {1,2,3,4,5,6,7};
+      int subLength = 3;
+      int[] subDivisor = {2,3,5,7,11,13,17};
+      boolean fullyDivisible = true;
+      for ( int j = 0; j < subStart.length; j++ )
+      {
+	 int endIndex = subStart[j] + subLength;
+	 int subDigit = 0;
+	 if ( endIndex >= digitStr.length() )
+	 {
+	    subDigit = Integer.parseInt(digitStr.substring(subStart[j]));
+	 }
+	 else
+	 {
+	    subDigit = Integer.parseInt(digitStr.substring(subStart[j],endIndex));
+	 }
+	 if ( subDigit % subDivisor[j] != 0 )
+	 {
+	    fullyDivisible = false;
+	    break;
+	 }
+      }
+      return fullyDivisible;
+   }
+ 
+   public long permute(String prefix, String str)
+   {
+      long sum = 0;
+      int len = str.length();
+      if ( len == 0 )
+      {
+	 // we are at a permutation since the suffix str is length 0;
+	 if ( isSubStringDivisibility(prefix) )
+	 {
+	    return Long.parseLong(prefix);
+         }
+      }
+      else
+      {
+         for ( int i = 0; i < len; i++ )
+         {
+            sum += permute(prefix + str.charAt(i), str.substring(0,i) + str.substring(i+1, len));
+         }
+      }
+      return sum;
+      
+   }
+ 
+   public long findPandigitalSubStringDivisibilitySum()
+   {
+      String digits =  "0123456789";
+      long sum = permute("",digits);
+      return sum;
+   }
+  
 
    public long findPandigitalPrime()
    {
@@ -36,9 +107,10 @@ public class Pandigital
       long largestPandigitalPrime = 0;
       for ( Long prime : primeArray )
       {	
-	 if ( isPandigital( prime.toString(), prime.toString().length() ) )
+	 
+	 if ( !containsZero(prime.toString()) && isPandigital( prime.toString(), prime.toString().length(), 1 ) )
 	 {
-	    System.out.println("Found Pandigital Prime: " + prime );
+	    //System.out.println("Found Pandigital Prime: " + prime );
 	    if ( prime.longValue() > largestPandigitalPrime )
 	    {
 	       largestPandigitalPrime = prime.longValue();
@@ -61,7 +133,8 @@ public class Pandigital
 	    if ( !containsZero(String.valueOf(cur)) )
 	    {
 	       bld.append(cur);
-	       if ( bld.length() == 9 && isPandigital(bld.toString(),9) )
+
+	       if ( !containsZero(bld.toString()) && bld.length() == 9 && isPandigital(bld.toString(),9,1) )
 	       {
 		  long curPandigital = Long.parseLong(bld.toString());
 		  if ( curPandigital > maxPandigitalMultiple )
@@ -119,8 +192,11 @@ public class Pandigital
       bld.append(j);
       bld.append((i*j));
       
-      
-      boolean isPandigital = isPandigital(bld.toString(),9);
+      boolean isPandigital = false;
+      if ( !containsZero(bld.toString() ) )
+      { 
+	 isPandigital = isPandigital(bld.toString(),9,1);
+      }
       
       if ( isPandigital )
       {
@@ -130,12 +206,11 @@ public class Pandigital
       
    }
 
-   public boolean isPandigital(String num, int n)
+   public boolean isPandigital(String num, int n, int start)
    {
-      java.util.HashMap<Integer,Integer> digitMap = buildHashMap(n);										      
-      Integer zeroVal = Integer.valueOf(0);
+      java.util.HashMap<Integer,Boolean> digitMap = buildHashMap(n, start);  
       boolean isPandigital = false;
-      if ( !containsZero(num) && num.length() == n )
+      if ( num.length() == n )
       {
          isPandigital = true;
          for ( char x : num.toCharArray() )
@@ -143,9 +218,9 @@ public class Pandigital
 	    
             String tmp = String.valueOf(x);
             Integer val = Integer.parseInt(tmp);
-	    if ( digitMap.get(val) != null &&  digitMap.get(val).equals(zeroVal) )
+	    if ( digitMap.get(val) != null &&  digitMap.get(val).equals(Boolean.FALSE) )
             {
-               digitMap.put(val,Integer.valueOf(1));
+               digitMap.put(val,Boolean.TRUE);
             }
             else
             {
@@ -157,12 +232,12 @@ public class Pandigital
       return isPandigital;
    }
 
-   public java.util.HashMap<Integer,Integer> buildHashMap(int n)
+   public java.util.HashMap<Integer,Boolean> buildHashMap(int n,int start)
    {
-      java.util.HashMap<Integer,Integer> theMap = new java.util.HashMap<Integer,Integer>();
-      for ( int i = 1; i <= n; i++ )
+      java.util.HashMap<Integer,Boolean> theMap = new java.util.HashMap<Integer,Boolean>();
+      for ( int i = start; i <= n; i++ )
       {
-	 theMap.put(Integer.valueOf(i),Integer.valueOf(0));
+	 theMap.put(Integer.valueOf(i),Boolean.FALSE);
       }
       return theMap;
    }
